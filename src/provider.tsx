fd7b3b8fc65de6ab7fe43641c81c6ecb7f09c7c4
Nikita Sviridenko <nikita.sviridenko@voypost.com>
2020-12-14 12:43:05 +0100

    Support custom rendering functions

--- src/provider.tsx
+++ src/provider.tsx
@@ -2,23 +2,35 @@ import { StyleProp, View, ViewStyle } from "react-native";
 import * as React from "react";
 import { publish } from "./pubsub";
 
+type TouchTrackerCaptureHandler = (evt: any) => {};
+
+interface TouchTrackerProviderRendererParams = {
+   handleStartShouldSetResponderCapture: TouchTrackerCaptureHandler;
+};
+
 type TouchTrackerProviderProps = {
   children: React.ReactNode;
+  renderer: ({ handleStartShouldSetResponderCapture }: TouchTrackerProviderRendererParams) => React.ReactNode;
   style?: StyleProp<ViewStyle>;
 };
 
 export function TouchTrackerProvider({
   children,
+  renderer,
   style,
 }: TouchTrackerProviderProps): JSX.Element {
+  const handleStartShouldSetResponderCapture = (evt) => {
+    evt.persist(); // remove after react v17, e.persist()
+    publish(evt);
+    return false;
+  };
+  if (renderer) {
+    return renderer({ handleStartShouldSetResponderCapture });
+  }
   return (
     <View
       style={style}
-      onStartShouldSetResponderCapture={(evt) => {
-        evt.persist(); // remove after react v17, e.persist()
-        publish(evt);
-        return false;
-      }}
+      onStartShouldSetResponderCapture={handleStartShouldSetResponderCapture}
     >
       {children}
     </View>
